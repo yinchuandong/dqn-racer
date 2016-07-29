d3252edb961a2d57467e71f1d032977d669f7f82
yinchuandong <yincd520@sina.com>
2016-07-29 15:00:44 +1000

    finish js game interface design

---
+++ socket_ctrl.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from PIL import Image
+from io import BytesIO
+import base64
+import time
+
+# python xxx.py
+# 127.0,0.1:5000
+app = Flask(__name__, static_url_path='')
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+
+def getTime():
+    return int(round(time.time() * 1000))
+
+
+@socketio.on('init')
+def handle_init(msg):
+    print msg
+    action = {'keyLeft': False, 'keyRight': False, 'keyFaster': True, 'keySlower': False}
+    emit('init', action)
+    return
+
+
+@socketio.on('message')
+def handle_message(msg):
+    # send('received message: ' + message)
+    print 'received message: '
+    # print msg
+    print msg['status']
+    image = Image.open(BytesIO(base64.b64decode(msg['img'])))
+    imgname = 'img/%s.png' % getTime()
+    image.save(imgname)
+    # action = agent.step(transition)
+    # return action
+    return
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('v4.final.html')
+
+if __name__ == '__main__':
+    socketio.run(app)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ static/socket_control.js
@@ -0,0 +1,101 @@
+(function(){
+  var socket = io('http://127.0.0.1:5000');
+  var jStatus = Dom.get('server-status');
+
+  socket.on('connect', function(){
+    jStatus.innerHTML = '<p>connect</p>';
+  });
+  
+  socket.on('disconnect', function(){
+    jStatus.innerHTML += '<p>disconnect</p>';
+  });
+
+  socket.on('init', function(action){
+    timeIntervalID = setInterval(function(){
+      var json = capture();
+      socket.emit('message', json);
+    }, 1000/30);
+  });
+
+  socket.on('message', function(action){
+
+  });
+
+  var timeIntervalID = null;
+  // dynamically create a smaller canvas for preview
+  var smallCanvas = document.createElement('canvas');
+  var smallCtx = smallCanvas.getContext('2d');
+  var smallImage = new Image();
+  var smallWidth = 84;
+  var smallHeight = 84;
+  smallCanvas.width = smallWidth;
+  smallCanvas.height = smallHeight;
+  smallCanvas.zIndex = '100';
+  Dom.get('preview').appendChild(smallCanvas);
+
+  // scale the snapshot of main canvas to a smaller one
+  function scaleImage(){
+    smallImage.src = canvas.toDataURL();
+    smallCtx.drawImage(smallImage, 0, 0, smallWidth, smallHeight);
+    var prefix = 'data:image/png;base64,';
+    return smallCanvas.toDataURL().substr(prefix.length);
+  }
+
+  function capture() {
+    var data = scaleImage();
+    // send status to sever for calculating reward
+    var carStatus = {
+      collision: COLLISION_OCCURED,
+      playerX: playerX,
+      speed: speed,
+      maxSpeed: maxSpeed
+    };
+
+    var json = {
+      'img': data,
+      'status': carStatus,
+    }
+    console.log(COLLISION_OCCURED);
+    return json;
+  };
+
+  setTimeout(function(){
+    Game.run(gameParams);
+    var json = capture();
+    socket.emit('init', json);
+  }, 1000);
+
+//=========================================================================
+// THE GAME LOOP
+//=========================================================================
+  var gameParams = {
+    canvas: canvas, render: render, update: update, stats: stats, step: step,
+    images: ["background", "sprites"],
+    keys: [
+      { keys: [KEY.LEFT,  KEY.A], mode: 'down', action: function() { keyLeft   = true;  } },
+      { keys: [KEY.RIGHT, KEY.D], mode: 'down', action: function() { keyRight  = true;  } },
+      { keys: [KEY.UP,    KEY.W], mode: 'down', action: function() { keyFaster = true;  } },
+      { keys: [KEY.DOWN,  KEY.S], mode: 'down', action: function() { keySlower = true;  } },
+      { keys: [KEY.LEFT,  KEY.A], mode: 'up',   action: function() { keyLeft   = false; } },
+      { keys: [KEY.RIGHT, KEY.D], mode: 'up',   action: function() { keyRight  = false; } },
+      { keys: [KEY.UP,    KEY.W], mode: 'up',   action: function() { keyFaster = false; } },
+      { keys: [KEY.DOWN,  KEY.S], mode: 'up',   action: function() { keySlower = false; } }
+    ],
+    ready: function(images) {
+      background = images[0];
+      sprites    = images[1];
+      reset();
+      Dom.storage.fast_lap_time = Dom.storage.fast_lap_time || 180;
+      updateHud('fast_lap_time', formatTime(Util.toFloat(Dom.storage.fast_lap_time)));
+    },
+    afterUpdate: function(){ 
+      // if collision occured, restart the game
+      if (COLLISION_OCCURED){
+        var json = capture();
+        socket.emit('message', json);
+        Game.restart();
+      }
+    }
+  };
+
+})();
