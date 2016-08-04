493add43a8920b5e97e067c3e652d3abb042b21f
yinchuandong <yincd520@sina.com>
2016-08-04 12:05:58 +0800

    fix: removing the maxpooling layer

--- dqn.py
+++ dqn.py
@@ -71,11 +71,11 @@ class DQN(object):
         W_conv1 = weight_variable([8, 8, INPUT_CHANNEL, 32], name='W_conv1')
         b_conv1 = bias_variable([32], name='b_conv1')
         h_conv1 = tf.nn.relu(conv2d(s, W_conv1, 4) + b_conv1)
-        h_pool1 = max_pool_2x2(h_conv1)
+        # h_pool1 = max_pool_2x2(h_conv1)
 
         W_conv2 = weight_variable([4, 4, 32, 64], name='W_conv2')
         b_conv2 = bias_variable([64], name='b_conv2')
-        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
+        h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, 2) + b_conv2)
 
         W_conv3 = weight_variable([3, 3, 64, 64], name='W_conv3')
         b_conv3 = bias_variable([64], name='b_conv3')
