f26739752ce3edf24cdb79faab2b7ac92e134b00
yinchuandong <johncdyin@gmail.com>
2016-10-02 16:41:34 +1100

    update: change W_action to fc_1 layer

--- ddpg/critic_network.py
+++ ddpg/critic_network.py
@@ -45,9 +45,7 @@ class CriticNetwork:
         # conv2 = state + action: concat action with conv at 2rd layer
         W_conv2 = weight_variable([4, 4, 32, 64])
         b_conv2 = bias_variable([64])
-        W_action2 = self.variable([self.action_dim, 64], h_conv1_out_size + action_dim)
-        h_action2 = tf.reshape(tf.matmul(action_input, W_action2), [-1, 1, 1, 64])
-        h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, 2) + h_action2 + b_conv2)
+        h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, 2) + b_conv2)
 
         # conv3
         W_conv3 = weight_variable([3, 3, 64, 64])
@@ -61,14 +59,15 @@ class CriticNetwork:
         # fc1
         W_fc1 = weight_variable([h_conv3_out_size, 512])
         b_fc1 = bias_variable([512])
-        h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
+        W_action = self.variable([action_dim, 512], h_conv3_out_size + action_dim)
+        h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + tf.matmul(action_input, W_action) + b_fc1)
 
         # output q value, fuck. output is only 1-D, instead of action_dim
         W_fc2 = weight_variable([512, 1])
         b_fc2 = bias_variable([1])
         q_value_output = tf.identity(tf.matmul(h_fc1, W_fc2) + b_fc2)
 
-        params = [W_conv1, b_conv1, W_conv2, W_action2, b_conv2, W_conv3, b_conv3, W_fc1, b_fc1, W_fc2, b_fc2]
+        params = [W_conv1, b_conv1, W_conv2, b_conv2, W_conv3, b_conv3, W_fc1, b_fc1, W_fc2, b_fc2, W_action]
         return state_input, action_input, q_value_output, params
 
     def create_target_q_network(self, net):
@@ -83,18 +82,15 @@ class CriticNetwork:
         target_net = [ema.average(x) for x in net]
 
         h_conv1 = tf.nn.relu(conv2d(state_input, target_net[0], 4) + target_net[1])
-        h_action2 = tf.reshape(tf.matmul(action_input, target_net[3]), [-1, 1, 1, 64])
-        h_conv2 = tf.nn.relu(conv2d(h_conv1, target_net[2], 2) + h_action2 + target_net[4])
-        # h_conv2 = tf.nn.relu(conv2d(h_conv1, target_net[2], 2) +
-        # tf.matmul(action_input, target_net[3]) + target_net[4])
-        h_conv3 = tf.nn.relu(conv2d(h_conv2, target_net[5], 1) + target_net[6])
+        h_conv2 = tf.nn.relu(conv2d(h_conv1, target_net[2], 2) + target_net[3])
+        h_conv3 = tf.nn.relu(conv2d(h_conv2, target_net[4], 1) + target_net[5])
 
         h_conv3_out_size = np.prod(h_conv3.get_shape().as_list()[1:])
         print 'critic: h_target_conv3_out_size', h_conv3_out_size
         h_conv3_flat = tf.reshape(h_conv3, [-1, h_conv3_out_size])
 
-        h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, target_net[7]) + target_net[8])
-        q_value_output = tf.identity(tf.matmul(h_fc1, target_net[9]) + target_net[10])
+        h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, target_net[6]) + tf.matmul(action_input, target_net[10]) + target_net[7])
+        q_value_output = tf.identity(tf.matmul(h_fc1, target_net[8]) + target_net[9])
         return state_input, action_input, q_value_output, target_update
 
     def create_training_method(self):
